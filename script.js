eb5fa96c1e86c357f8979959bd447d90f47d25d0
Amal Jama <amal_839@hotmail.com>
2022-11-30 22:45:27 -0500

    changed etch a sketch ui

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // script.js
+
+// drawing-pad is the white box where you will draw
+// greyscale, pastel, neutral, and clear are the four buttons
+
+
+let grid = document.getElementById('drawing-pad');
+
+// want to create a grid
+// defined column and row sizes in css
+// then create elements in javascript and put them there
